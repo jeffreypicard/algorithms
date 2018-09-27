b8c9bfa6939ca44187044d20eceab87d63c72c60
Jeffrey Picard <jeff@jeffreypicard.com>
2018-09-26 20:28:06 -0400

    Initial main program for topoligical sort.

---
+++ top_srt.c
@@ -0,0 +1,16 @@
+/*
+ * top_srt.c
+ *
+ * Academic implementation of topological sort on a graph.
+ *
+ * Author: Jeffrey Picard (jeff@jeffreypicard.com)
+ */
+
+int
+main(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+
+    return -1;
+}
